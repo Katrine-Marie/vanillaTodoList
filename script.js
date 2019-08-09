4d93631d845a053ba38e0f3d47d1f8f005b33d5f
fjordscene@gmail.com <fjordscene@gmail.com>
2019-08-09 11:31:40 +0200

    Removed comments from before localstorage implementation

--- script.js
+++ script.js
@@ -2,11 +2,6 @@ class Model {
   constructor() {
     // The state of the model, an array of todo objects, prepopulated with some data
 
-		/*this.todos = [
-      { id: 1, text: 'Run a marathon', complete: false },
-      { id: 2, text: 'Plant a garden', complete: false },
-    ]*/
-
 		this.todos = JSON.parse(localStorage.getItem('todos')) || []
 
   }
